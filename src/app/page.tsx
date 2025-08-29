3b59484c5a5368b6fdc0da7a991e1517a5f31df1
Shivam <devshivam018@gmail.com>
2025-08-30 00:49:34 +0530

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 import { Footer } from "../../components/footer";
 import Header from "../../components/header";
 import { Timer } from "../../components/timer";
-import HeroSection from "./component";
-import { ProjectSection } from "./routeComponents";
+import HeroSection, { ProjectSection, Work } from "./routeComponents";
 
 export default function Home() {
     return (
         <>
             <Header />
             <HeroSection />
-             <div className="h-28"></div>
+             <div className="h-10 md:h-28"></div>
             <ProjectSection/>
-            <div className="h-10"></div>
-            <Timer />
+            <div className="h-10 md:h-28"></div>
+            <Work/>
+            {/* <Timer /> */}
             <Footer />
         </>
     );
--- src/app/routeComponents.tsx
+++ src/app/routeComponents.tsx
@@ -1,5 +1,127 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import GitHubCalendar from "react-github-calendar";
+
+export default function HeroSection() {
+    return (
+        <div className="max-w-6xl px-4 lg:px-8 mx-auto">
+            {/* <Image
+        src={
+          "https://res.cloudinary.com/duwfzddrs/image/upload/v1748460158/s-white-logo_wp2nbw.svg"
+        }
+        width={20}
+        height={20}
+        alt="s-logo"
+      /> */}
+            <section className="flex gap-12 flex-wrap my-16">
+                <div className="max-w-2xl lg:w-[42rem]">
+                    <h1 className="text-5xl font-bold tracking-tight mb-3 dark:text-white">
+                        Software developer, designer.
+                    </h1>
+                    <p className="text-xl font-medium dark:font-normal text-zinc-700 leading-relaxed dark:text-zinc-400">
+                        Welcome to my corner of the internet! I{"'"}m Shivam, a software
+                        engineer and the author/maintainer of some exciting projects. This page is a
+                        list of my creations and musings, you can also find me on Twitter (
+                        <a
+                            href="https://x.com/ShivamG50827915"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="dark:text-zinc-300 text-zinc-800 underline"
+                        >
+                            @ShivamG50827915
+                        </a>
+                        ) and{" "}
+                        <a
+                            href="https://github.com/Shivamgautam043"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="dark:text-zinc-300 text-zinc-800 underline"
+                        >
+                            GitHub
+                        </a>{" "}
+                        for all things tech and beyond.
+                    </p>
+                </div>
+
+                <div className="max-w-80 relative">
+                    <div className="rounded-lg shadow-lg border border-gray-100 dark:border-gray-100/10 rotate-2 p-2 dark:bg-zinc-900 bg-white lg:max-w-72 w-full">
+                        <img
+                            src="https://shivam.dev/_astro/me.gUlH0Jun_247FUu.webp"
+                            alt="Shivam, standing on a cliff facing the Himalayas."
+                            width={400}
+                            height={400}
+                            className="w-auto aspect-square rounded overflow-hidden"
+                        />
+                        <div className="font-display lg:pt-2 lg:pb-0 pt-5 pb-2 text-3xl text-center dark:text-white">
+                            Auli, 2024
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <div className="max-md:hidden grid grid-cols-1 w-full place-content-center place-items-center">
+                <div className="scale-75">
+                    <GitHubCalendar username="Shivamgautam043" />
+                </div>
+            </div>
+            {/* <div className="w-full relative">
+        <img
+          src="https://res.cloudinary.com/duwfzddrs/image/upload/v1748461657/neo-desk_yi30jn.jpg"
+          alt=""
+          className="w-full"
+        />
+        <div className="absolute top-4 left-4">
+          <MatrixText/>
+        </div>
+      </div> */}
+        </div>
+    );
+}
+
+export function MatrixText() {
+    const [cursor, setCursor] = useState(true);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCursor((prev) => !prev);
+        }, 500);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const text = "The Matrix has you...";
+    const [modifiedText, setModifiedText] = useState("");
+    const indexRef = useRef(0);
+    const directionRef = useRef("increasing");
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const currentIndex = indexRef.current;
+            const direction = directionRef.current;
+
+            if (direction === "increasing") {
+                const nextIndex = currentIndex + 1;
+                setModifiedText(text.slice(0, nextIndex));
+                indexRef.current = nextIndex;
+
+                if (nextIndex === text.length) {
+                    directionRef.current = "decreasing";
+                }
+            } else {
+
+            }
+        }, 150);
+
+        return () => clearInterval(interval); // Cleanup
+    }, []);
+    return (
+        <div className="text-[#03A062] grid grid-flow-col place-content-start place-items-center">
+            <div className={`h-6 w-[0.1px]`}></div>
+            {modifiedText}{" "}
+            <div
+                className={`${cursor === true ? "w-2" : "w-0"} h-4 bg-[#03A062]`}
+            ></div>
+        </div>
+    );
+}
 
 export function ProjectSection() {
     return (
@@ -24,7 +146,6 @@ export function ProjectSection() {
         </>
     )
 }
-
 
 type Project = {
     title: string;
@@ -105,8 +226,8 @@ function ProjectCard({
     const [isHover, setIsHover] = useState(false);
     return (
         <div
-            onMouseEnter={()=>setIsHover(true)}
-            onMouseLeave={()=>setIsHover(false)}
+            onMouseEnter={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)}
             style={{
                 ["--project-color" as any]: color,
                 ["--mouse-x" as any]: "235px",
@@ -123,7 +244,7 @@ function ProjectCard({
             >
                 <div className="flex items-start justify-between">
                     <div className="flex items-center gap-2 relative">
-                        <img src={logo} alt={title} className="w-auto h-8 rounded-md overflow-hidden transition-all duration-300"   style={{scale:isHover===true?1.25:1}}/>
+                        <img src={logo} alt={title} className="w-auto h-8 rounded-md overflow-hidden transition-all duration-300" style={{ scale: isHover === true ? 1.25 : 1 }} />
                         <img
                             src={logo}
                             alt={`${title} blur`}
@@ -173,3 +294,104 @@ function ProjectCard({
         </div>
     );
 }
+
+const workData = [
+  {
+    logo: "/work/growthjockey.png",
+    title: "Growth Jockey",
+    tags: ["Software Developer-1", "Full-time"],
+    role: "Jul 2024 – Present · Gurugram, India",
+  },
+  {
+    logo: "/work/growthjockey.png",
+    title: "Growth Jockey",
+    tags: ["Associate - SDE", "Full-time"],
+    role: "Jan 2024 – Jun 2024 · Gurugram, India",
+  },
+  {
+    logo: "/work/moviebuff.png",
+    title: "Movie Buff",
+    tags: ["ReactJS", "NodeJS", "Firebase"],
+    role: "Personal Project · 2023",
+  },
+  {
+    logo: "/work/mediconnect.png",
+    title: "MediConnect",
+    tags: ["Remix", "React", "PostgreSQL"],
+    role: "Personal Project · 2024",
+  },
+  {
+    logo: "/work/sparks.png",
+    title: "The Sparks Foundation",
+    tags: ["Web Development Intern"],
+    role: "Jun 2023 – Jul 2023",
+  },
+];
+
+
+
+export function Work() {
+    return (
+        <>
+            <div className="max-w-6xl mx-auto ">
+                <div className="max-w-2xl px-4 lg:px-8 text-left">
+                    <div className="max-w-2xl"> <h2 className="text-3xl font-bold tracking-tight mb-3 dark:text-white">Work</h2> <p className="text-lg text-zinc-700 leading-relaxed tracking-normal dark:text-zinc-400">
+                        Over the past two years, I’ve gained valuable experience working with fast-growing startups
+                        and diverse clients, building scalable platforms, booking systems, and engaging digital products.
+                        This journey has given me the opportunity to collaborate with talented teams, solve real-world challenges,
+                        and deliver solutions that drive both performance and user experience.
+                    </p> </div>
+                </div>
+                <div className="grid gap-12 mt-8 max-w-xl  px-4 lg:px-8 text-left">
+                    <div className="grid -mx-2 gap-4 group">
+                        {workData.map((item, index) => (
+                            <WorkCard
+                                key={index}
+                                logo={item.logo}
+                                title={item.title}
+                                tags={item.tags}
+                                role={item.role}
+                            />
+                        ))}
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+
+function WorkCard({ logo, title, tags, role }: { logo: string, title: string, tags: string[], role: string }) {
+    return (
+        <div className="rounded-lg bg-white dark:bg-zinc-900 border border-zinc-200 dark:border-zinc-800 shadow-lg p-4 flex items-start gap-3 hover:shadow-xl hover:-translate-y-0.5 hover:border-zinc-300 hover:dark:border-zinc-700 transition-all duration-200">
+            <img
+                loading="lazy"
+                src={logo}
+                height="40"
+                width="40"
+                className="dark:rounded-md dark:scale-110 overflow-hidden dark:bg-zinc-900"
+                alt={`Logo for ${title}`}
+            />
+            <div>
+                <div className="flex lg:flex-row flex-col items-start lg:items-center gap-2">
+                    <h3 className="font-medium leading-none dark:text-white">{title}</h3>
+                    <div className="flex gap-2">
+                        {tags.map((tag, idx) => (
+                            <span
+                                key={idx}
+                                className="text-zinc-500 uppercase tracking-widest dark:text-zinc-400 text-[10px] font-medium border dark:border-zinc-600 rounded-md px-1 py-1 leading-none"
+                            >
+                                {tag}
+                            </span>
+                        ))}
+                    </div>
+                </div>
+                <p className="leading-relaxed tracking-normal text-zinc-500 mt-1 dark:text-zinc-400">
+                    {role}
+                </p>
+            </div>
+        </div>
+    );
+}
+
